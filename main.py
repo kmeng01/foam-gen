d36bdb4ecab3135f8f4f1a0076ebc04940e886f1
Kevin Meng <kevin.meng.01@gmail.com>
2023-06-01 18:50:26 -0500

    New keyboard controller

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ def image_to_svg(image_path, svg_path):
     dwg.save()
 
 
-# image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-hQq8ktL14LcqgFbmHicl29Z3/user-69gGbK3KFTykHOUkFZvglu52/img-AadcqQF1HpQpuQyJzxLUiiNM.png?st=2023-06-01T21%3A22%3A50Z&se=2023-06-01T23%3A22%3A50Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-06-01T21%3A01%3A17Z&ske=2023-06-02T21%3A01%3A17Z&sks=b&skv=2021-08-06&sig=exeKRrxI11uvmGx19xhaVU8gffYb7EXmC2oTY0V6nVI%3D"
-image_url = generate_image("Killian Court and the Dome at MIT")
+prompt = "A pirate eating a pineapple"
+image_url = generate_image(f"Simple line drawing of {prompt}")
 print(image_url)
 
 image_to_svg(image_url, "output.svg")
