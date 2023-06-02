6353f2bcab4ff1c0f9b86516fcc3e6abb599a57d
Kevin Meng <kevin.meng.01@gmail.com>
2023-06-01 19:32:04 -0500

    INclude ngrok

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+ngrok http 8000 > ngrok.txt 2>&1 &
+ngrok_pid=$!
+echo "PID of ngrok process: $ngrok_pid"
+
+sleep 2  # Wait for ngrok to establish the tunnel (adjust this as needed)
+
+ngrok_url=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "ngrok URL: $ngrok_url"
+
 python3 api.py
+
+
